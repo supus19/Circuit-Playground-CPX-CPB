5bf492dc27aab2c3e02c75d7a2f5250edb79b2f4
supus19 <55859540+supus19@users.noreply.github.com>
2025-01-14 08:50:43 -0800

    Update CPX-CPB-Blink-1.py

--- CPX-CPB-Blink-1.py
+++ CPX-CPB-Blink-1.py
@@ -6,6 +6,9 @@ import time
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+bobs_button_B = digitalio.DigitalInOut(board.Button_B)
+bobs_button_B.direction = digitalio.Direction.INPUT
+bobs_button_B.pull = digitalio.Pull.DOWN
 
 def main():
     print("Hello World")
@@ -14,4 +17,10 @@ def main():
         time.sleep(0.1)
         led.value = False
         time.sleep(0.1)
+    while True:
+        led.value = bobs_button_B.value
+        print(bobs_button_b.value)
+        time.sleep(.2)
+    print("Button_B Input/Output Example")
+    print("Push Button_B (Right Button)")
 main()
